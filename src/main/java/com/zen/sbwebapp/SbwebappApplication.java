33967b7231fabd4ff287c7db944b7c169b950a2a
AMotuang <alfredthabang@gmail.com>
2021-01-21 12:45:42 +0200

    modified sbwebapp

--- src/main/java/com/zen/sbwebapp/SbwebappApplication.java
+++ src/main/java/com/zen/sbwebapp/SbwebappApplication.java
@@ -8,6 +8,7 @@ public class SbwebappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbwebappApplication.class, args);
+		System.out.println("My name is Alfred");
 	}
 
 }
